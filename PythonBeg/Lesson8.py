e7e5c5164f41a4707f14802b50fc02b93f7c730e
Ihor Bardak <ihorbardak@Ihors-MacBook-Pro.local>
2022-06-07 09:25:56 +0300

    added_4_tasks

--- PythonBeg/Lesson8.py
+++ PythonBeg/Lesson8.py
@@ -1,5 +1,8 @@
 # Створити метод, який принімає строку і сивол, та вивести на екран, скільки разів цей символ повторюється в цій строчці
-
 sentence = str(input("Please enter your text: "))
 symb_count = str(input("Please enter symbol that need to be counted: "))
-print("Result:" + str(sentence.count(symb_count)))
+cnt = 0
+for counter in sentence:
+    if counter == symb_count:
+        cnt += 1
+print(cnt)
